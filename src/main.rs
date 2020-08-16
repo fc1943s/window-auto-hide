dcbe706283b084e633e0f76fb855807e2913ff9d
Filipe <fc1943s@gmail.com>
2020-08-16 12:59:24 -0300

    v0.1

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use pathsearch::find_executable_in_path;
+use std::fs;
+use eventual::{Timer};
+use windows_win::raw::window;
+use windows_win::winapi::um::winuser::{SW_HIDE};
+
+
+#[derive(serde::Deserialize)]
+pub struct Config {
+    class_name: String
+}
+
+fn hide_window(class_name: &String) {
+    let search_result = window::get_by_class(class_name, None).unwrap();
+
+    for window_hwnd in search_result {
+        println!("hiding handle {:?}", window_hwnd);
+        window::show(window_hwnd, SW_HIDE);
+    }
+}
+
+fn main() {
+    let config_path = find_executable_in_path("window-auto-hide.toml").expect("Config file not found");
+    let config_bytes = fs::read(config_path).expect("Error reading config file");
+    let config: Config = toml::from_slice(&config_bytes).expect("Error parsing config file");
+
+    let interval = 1000;
+
+    let timer = Timer::new();
+    let ticks = timer.interval_ms(interval).iter();
+
+    hide_window(&config.class_name);
+    for _ in ticks {
+        hide_window(&config.class_name);
+    }
+}
